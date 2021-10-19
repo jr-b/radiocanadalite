276bcc425578f71049f80233a66acb3c4496b2a0
jrb <jrb@jrb.lan>
2021-10-19 16:41:07 -0400

    filters for buildtime

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,15 @@
+const filters = require('./utils/filters.js')
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css/");
   eleventyConfig.addWatchTarget("./src/css/");
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+    // Filters
+    Object.keys(filters).forEach((filterName) => {
+      eleventyConfig.addFilter(filterName, filters[filterName])
+    })
 
   return {
     dir: {
