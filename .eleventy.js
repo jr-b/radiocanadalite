970b3f5713780a03f4bbb25b8a40da091890691a
jrb <you@example.com>
2021-11-01 11:11:05 -0400

    css adjusments on article page

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,13 @@
 const filters = require('./utils/filters.js')
+const pluginDate = require("eleventy-plugin-date");
+
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/scss/");
   eleventyConfig.addWatchTarget("./src/scss/");
   eleventyConfig.addPassthroughCopy("./utils/");
   eleventyConfig.addPassthroughCopy({"src/static": "/"});
+  eleventyConfig.addPlugin(pluginDate);
 
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
