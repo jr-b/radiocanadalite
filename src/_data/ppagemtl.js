5c0340af1a89536a2e43c39251bcb414b4930b64
jr-b <j@jrb.nz>
2022-10-03 21:19:37 -0400

    debug front page: logs activated for ppagemtl data

--- src/_data/ppagemtl.js
+++ src/_data/ppagemtl.js
@@ -9,7 +9,7 @@ async function fetchNews() {
     ]);
     const page1Json = await page1.json();
     const pag2Json = await page2.json();
-    //console.log([page1, page2]);
+    console.log([page1, page2]);
     
     // keeping only the selfLink and the codeName, page 1
     let newsIdArray1 = page1Json.contentItemSummaries.items.map(id => {
@@ -30,13 +30,13 @@ async function fetchNews() {
     // concat the two pages
     let newsIdFull = newsIdArray1.concat(newsIdArray2);
     
-       //console.log(newsIdFull);
+       console.log(newsIdFull);
     
     // looping over every item, and fetching its full endpoint
     for (const newsData of newsIdFull){
         if (newsData.id !== null){    
         let url = `${newsData.id.href}`;
-            // console.log(url);
+             console.log(url);
             let newsDataRequest = await fetch(url);
             let data = await newsDataRequest.json();
             newsData.address = data;
