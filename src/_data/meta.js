61eece532412b4b5ee529355020bc983aa00d1a0
jr-b <j@jrb.nz>
2022-10-03 20:38:13 -0400

    sitemap and robots.txt update

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,9 +1,9 @@
 module.exports = {
   // NOTE: `process.env.URL` is provided by Netlify, and may need
   // adjusted pending your host
-  url: "https://rc-lite.xyz",
+  url: "https://radiocanadamini.ca",
   siteName: "Radio-Canada Mini",
   siteDescription:
     "Une version légère de Radio-Canada.ca",
-  authorName: "Jérémi Robitaille-Brassard", // optional
+  authorName: "Jérémi Robitaille", // optional
 };
