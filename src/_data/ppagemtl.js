8e4b9490328945c95a4a3b86fa51daade148c898
jr-b <j@jrb.nz>
2023-06-19 11:29:27 -0400

    fix ppage content, fetch with sphere

--- src/_data/ppagemtl.js
+++ src/_data/ppagemtl.js
@@ -1,3 +1,71 @@
+// const fetch = require("node-fetch");
+
+// // https://services.radio-canada.ca/neuro/sphere/v1/lineups/4159?pageNumber=2
+
+// async function fetchAndConcatenateJSON(url) {
+//   let concatenatedJSON = [];
+
+//   let currentPage = 1;
+//   let lastPage = false;
+
+//   // call each page available
+//   // build concatenatedJSON array with the items returned
+//   while (!lastPage) {
+//     let tempUrl = url + "?pageNumber=" + currentPage;
+//     const response = await fetch(tempUrl);
+//     const json = await response.json();
+//     //console.log(json);
+
+//     concatenatedJSON.push(...json.items);
+
+//     const { pagedConfiguration } = json;
+//     if (pagedConfiguration.nextPageUrl === null) {
+//       lastPage = true;
+//     } else {
+//       currentPage++;
+//     }
+//   }
+
+//   //console.log(concatenatedJSON);
+
+//   // build list of news id into an array
+//   let newsIdArray = concatenatedJSON.map((item) => {
+
+//     return {
+//       id: item.id,
+//       type: item.contentTypeId
+//     };
+//   });
+
+//   //console.log(newsIdArray);
+
+//   let fullNews = [];
+//   // looping over every item, and fetching its full data
+//   for (const newsData of newsIdArray) {
+//     if (newsData.type == "NewsStory" && newsData.type !== null) {
+//       let newsUrl = "https://services.radio-canada.ca/neuro/v1/news-stories/" + newsData.id;
+//       //console.log(newsUrl);
+//       const newsDataRequest = await fetch(newsUrl);
+//       const dataJson = await newsDataRequest.json();
+//       //console.log(dataJson);
+//       fullNews.push(dataJson);
+//     }
+//   }
+
+//   return fullNews;
+// }
+
+// let urlList = "https://services.radio-canada.ca/neuro/v1/lineups/4159";
+
+// module.exports = fetchAndConcatenateJSON(urlList)
+//   .then((fullNews) => {
+//     return fullNews;
+//   })
+//   .catch((error) => {
+//     console.error("Error:", error);
+//   });
+
+
 const fetch = require("node-fetch");
 
 async function fetchAndConcatenateJSON(url) {
@@ -6,36 +74,35 @@ async function fetchAndConcatenateJSON(url) {
   let currentPage = 1;
   let lastPage = false;
 
-  // call each page available
-  // build concatenatedJSON array with the items returned
   while (!lastPage) {
-    let tempUrl = url + "?pagenumber=" + currentPage;
+    let tempUrl = url + "?pageNumber=" + currentPage;
     const response = await fetch(tempUrl);
     const json = await response.json();
 
-    concatenatedJSON.push(...json.contentItemSummaries.items);
+    if (json.items) {
+      concatenatedJSON.push(...json.items);
+    }
 
-    const { contentItemSummaries } = json;
-    if (contentItemSummaries.nextPageLink === null) {
+    const { pagedConfiguration } = json;
+    if (pagedConfiguration.nextPageUrl === null) {
       lastPage = true;
     } else {
       currentPage++;
     }
   }
 
-  // build list of news id into an array
   let newsIdArray = concatenatedJSON.map((item) => {
     return {
-      id: item.selfLink,
-      type: item.classificationTag.codeName,
+      id: item.id,
+      type: item.contentTypeId,
     };
   });
 
   let fullNews = [];
-  // looping over every item, and fetching its full data
   for (const newsData of newsIdArray) {
-    if (newsData.id !== null) {
-      let newsUrl = `${newsData.id.href}`;
+    if (newsData.type === "NewsStory" && newsData.id !== null) {
+      let newsUrl =
+        "https://services.radio-canada.ca/neuro/v1/news-stories/" + newsData.id;
       const newsDataRequest = await fetch(newsUrl);
       const dataJson = await newsDataRequest.json();
       fullNews.push(dataJson);
@@ -45,12 +112,16 @@ async function fetchAndConcatenateJSON(url) {
   return fullNews;
 }
 
-let urlList = "https://services.radio-canada.ca/neuro/v1/lineups/4159";
+async function getFullNewsData() {
+  try {
+    let urlList = "https://services.radio-canada.ca/neuro/sphere/v1/lineups/4159";
+    const fullNews = await fetchAndConcatenateJSON(urlList);
 
-module.exports = fetchAndConcatenateJSON(urlList)
-  .then((fullNews) => {
     return fullNews;
-  })
-  .catch((error) => {
+
+  } catch (error) {
     console.error("Error:", error);
-  });
+  }
+}
+
+module.exports = getFullNewsData();
