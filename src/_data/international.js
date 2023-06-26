c603826f061aa29c4ff45ec119cd8aad48244620
jr-b <j@jrb.nz>
2023-06-25 23:16:23 -0400

    fix: duplicates in lineup

--- src/_data/international.js
+++ src/_data/international.js
@@ -1,3 +1,4 @@
+
 const fetch = require("node-fetch");
 
 async function fetchAndConcatenateJSON(url) {
@@ -6,14 +7,18 @@ async function fetchAndConcatenateJSON(url) {
   let currentPage = 1;
   let lastPage = false;
 
-  // call each page available
-  // build concatenatedJSON array with the items returned
+  function onlyUnique(value, index, array) {
+    return array.indexOf(value) === index;
+  }
+
   while (!lastPage) {
     let tempUrl = url + "?pagenumber=" + currentPage;
     const response = await fetch(tempUrl);
     const json = await response.json();
 
-    concatenatedJSON.push(...json.contentItemSummaries.items);
+    if (json.contentItemSummaries.items) {
+      concatenatedJSON.push(...json.contentItemSummaries.items);
+    }
 
     const { contentItemSummaries } = json;
     if (contentItemSummaries.nextPageLink === null) {
@@ -23,19 +28,21 @@ async function fetchAndConcatenateJSON(url) {
     }
   }
 
-  // build list of news id into an array
   let newsIdArray = concatenatedJSON.map((item) => {
     return {
-      id: item.selfLink,
+      id: item.id,
       type: item.classificationTag.codeName,
     };
   });
 
+  let filteredNewsIdArray = newsIdArray.filter(onlyUnique);
+  console.log(filteredNewsIdArray);
+
   let fullNews = [];
-  // looping over every item, and fetching its full data
-  for (const newsData of newsIdArray) {
-    if (newsData.id !== null) {
-      let newsUrl = `${newsData.id.href}`;
+  for (const newsData of filteredNewsIdArray) {
+    if (newsData.type === "nouvelle" && newsData.id !== null) {
+      let newsUrl =
+        "https://services.radio-canada.ca/neuro/v1/news-stories/" + newsData.id;
       const newsDataRequest = await fetch(newsUrl);
       const dataJson = await newsDataRequest.json();
       fullNews.push(dataJson);
@@ -45,7 +52,7 @@ async function fetchAndConcatenateJSON(url) {
   return fullNews;
 }
 
-let urlList = "https://services.radio-canada.ca/neuro/v1/lineups/96";
+let urlList = "https://services.radio-canada.ca/neuro/v1/lineups/92411";
 
 module.exports = fetchAndConcatenateJSON(urlList)
   .then((fullNews) => {
@@ -54,3 +61,69 @@ module.exports = fetchAndConcatenateJSON(urlList)
   .catch((error) => {
     console.error("Error:", error);
   });
+
+
+
+// const fetch = require("node-fetch");
+
+// async function fetchAndConcatenateJSON(url) {
+//   let concatenatedJSON = [];
+
+//   let currentPage = 1;
+//   let lastPage = false;
+
+//   function onlyUnique(value, index, array) {
+//     return array.indexOf(value) === index;
+//   }
+
+//   // call each page available
+//   // build concatenatedJSON array with the items returned
+//   while (!lastPage) {
+//     let tempUrl = url + "?pagenumber=" + currentPage;
+//     const response = await fetch(tempUrl);
+//     const json = await response.json();
+
+//     concatenatedJSON.push(...json.contentItemSummaries.items);
+
+//     const { contentItemSummaries } = json;
+//     if (contentItemSummaries.nextPageLink === null) {
+//       lastPage = true;
+//     } else {
+//       currentPage++;
+//     }
+//   }
+
+//   // build list of news id into an array
+//   let newsIdArray = concatenatedJSON.map((item) => {
+//     return {
+//       id: item.selfLink,
+//       type: item.classificationTag.codeName,
+//     };
+//   });
+
+//   let filteredNewsIdArray = [...new Set(newsIdArray)];
+//   console.log(filteredNewsIdArray);
+
+//   let fullNews = [];
+//   // looping over every item, and fetching its full data
+//   for (const newsData of filteredNewsIdArray) {
+//     if (newsData.id !== null) {
+//       let newsUrl = `${newsData.id.href}`;
+//       const newsDataRequest = await fetch(newsUrl);
+//       const dataJson = await newsDataRequest.json();
+//       fullNews.push(dataJson);
+//     }
+//   }
+
+//   return fullNews;
+// }
+
+// let urlList = "https://services.radio-canada.ca/neuro/v1/lineups/96";
+
+// module.exports = fetchAndConcatenateJSON(urlList)
+//   .then((fullNews) => {
+//     return fullNews;
+//   })
+//   .catch((error) => {
+//     console.error("Error:", error);
+//   });
--- src/_data/ppagemtl.js
+++ src/_data/ppagemtl.js
@@ -1,69 +1,4 @@
-// const fetch = require("node-fetch");
 
-// // https://services.radio-canada.ca/neuro/sphere/v1/lineups/4159?pageNumber=2
-
-// async function fetchAndConcatenateJSON(url) {
-//   let concatenatedJSON = [];
-
-//   let currentPage = 1;
-//   let lastPage = false;
-
-//   // call each page available
-//   // build concatenatedJSON array with the items returned
-//   while (!lastPage) {
-//     let tempUrl = url + "?pageNumber=" + currentPage;
-//     const response = await fetch(tempUrl);
-//     const json = await response.json();
-//     //console.log(json);
-
-//     concatenatedJSON.push(...json.items);
-
-//     const { pagedConfiguration } = json;
-//     if (pagedConfiguration.nextPageUrl === null) {
-//       lastPage = true;
-//     } else {
-//       currentPage++;
-//     }
-//   }
-
-//   //console.log(concatenatedJSON);
-
-//   // build list of news id into an array
-//   let newsIdArray = concatenatedJSON.map((item) => {
-
-//     return {
-//       id: item.id,
-//       type: item.contentTypeId
-//     };
-//   });
-
-//   //console.log(newsIdArray);
-
-//   let fullNews = [];
-//   // looping over every item, and fetching its full data
-//   for (const newsData of newsIdArray) {
-//     if (newsData.type == "NewsStory" && newsData.type !== null) {
-//       let newsUrl = "https://services.radio-canada.ca/neuro/v1/news-stories/" + newsData.id;
-//       //console.log(newsUrl);
-//       const newsDataRequest = await fetch(newsUrl);
-//       const dataJson = await newsDataRequest.json();
-//       //console.log(dataJson);
-//       fullNews.push(dataJson);
-//     }
-//   }
-
-//   return fullNews;
-// }
-
-// let urlList = "https://services.radio-canada.ca/neuro/v1/lineups/4159";
-
-// module.exports = fetchAndConcatenateJSON(urlList)
-//   .then((fullNews) => {
-//     return fullNews;
-//   })
-//   .catch((error) => {
-//     console.error("Error:", error);
-//   });
 
 
 const fetch = require("node-fetch");
@@ -73,6 +8,10 @@ async function fetchAndConcatenateJSON(url) {
 
   let currentPage = 1;
   let lastPage = false;
+
+  function onlyUnique(value, index, array) {
+    return array.indexOf(value) === index;
+  }
 
   while (!lastPage) {
     let tempUrl = url + "?pageNumber=" + currentPage;
@@ -98,8 +37,10 @@ async function fetchAndConcatenateJSON(url) {
     };
   });
 
+  let filteredNewsIdArray = newsIdArray.filter(onlyUnique);
+
   let fullNews = [];
-  for (const newsData of newsIdArray) {
+  for (const newsData of filteredNewsIdArray) {
     if (newsData.type === "NewsStory" && newsData.id !== null) {
       let newsUrl =
         "https://services.radio-canada.ca/neuro/v1/news-stories/" + newsData.id;
